05e9e2fc0b05569a75a7a22897a2e3b578aa5f15
Fahrizaldi <45739897+aldmf26@users.noreply.github.com>
2025-12-04 15:18:42 +0800

    g

--- app/app.vue
+++ app/app.vue
@@ -4,9 +4,9 @@
   </UApp>
 </template>
 <script setup lang="ts">
-// app.vue atau layout/default.vue
+
 useSeoMeta({
-  titleTemplate: "Convert & Download Video • VidBox",
+  titleTemplate: "Convert & Download YouTube, TikTok, Instagram • Converter Aldi",
   description:
     "Download video YouTube, TikTok, Instagram tanpa watermark. Cepat, gratis, HD.",
   ogImage: "/og-image.jpg",
